9bcf1efc19588d9988b2603522dd408cc6bbaf17
lucwii <milanoviclukaa23@gmail.com>
2026-03-03 23:49:51 +0100

    feat: supabase setup

---
+++ backend/src/supabase/supabase.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { createClient } from '@supabase/supabase-js';
+
+@Injectable()
+export class SupabaseService {
+  private client;
+
+  constructor() {
+    this.client = createClient(
+      process.env.SUPABASE_URL!,
+      process.env.SUPABASE_SERVICE_ROLE_KEY!
+    );
+  }
+
+  get db() {
+    return this.client;
+  }
+}
