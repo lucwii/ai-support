44dbab1333026fc9d980fa1ad4da2db6e790693f
lucwii <milanoviclukaa23@gmail.com>
2026-03-11 21:27:59 +0100

    feat: cors added

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:3000',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
