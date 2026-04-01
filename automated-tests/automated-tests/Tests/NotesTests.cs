faa390f29cb8c67213d605d4dcb6503fa86fe3eb
lucwii <milanoviclukaa23@gmail.com>
2026-04-01 23:42:14 +0200

    feat: notes page tests added

---
+++ automated-tests/automated-tests/Tests/NotesTests.cs
@@ -0,0 +1,65 @@
+using automated_tests.Helpers;
+using automated_tests.Pages;
+
+namespace automated_tests.Tests;
+
+public class NotesTests : TestBase
+{
+    private TicketDetailPage ticketDetailPage;
+    
+    private const string AgentEmail = "test@gmail.com";
+    private const string AgentPassword = "Luka1310";
+    private const string TicketId = "b48a8608-c4a5-48e6-a660-f0f57566eaf8";
+    
+    [SetUp]
+    public void SetUp()
+    {
+        ticketDetailPage = new TicketDetailPage(driver);
+        ticketDetailPage.LoginAndNavigate(AgentEmail, AgentPassword, TicketId);
+    }
+
+    [Test]
+    public void NotesCard_ShouldBeVisible()
+    {
+        Assert.That(ticketDetailPage.IsNotesCardVisible());
+    }
+    
+    [Test]
+    public void EmptyState_ShouldBeVisible()
+    {
+        Assert.That(ticketDetailPage.IsEmptyStateVisible());
+    }
+
+    [Test]
+    public void AddNoteClick_ShouldOpenModal()
+    {
+        ticketDetailPage.ClickAddNote();
+        
+        Assert.That(ticketDetailPage.IsModalOpened());
+    }
+
+    [Test]
+    public void CancelButtonClick_ShouldCloseModal()
+    {
+        ticketDetailPage.ClickAddNote();
+        ticketDetailPage.ClickCancelButton();
+        
+        Assert.That(ticketDetailPage.IsModalOpened(), Is.Not.True);
+    }
+
+    [Test]
+    public void CreateNote_ShouldNotShowError()
+    {
+        int countBefore = ticketDetailPage.GetNoteItemsCount();
+        
+        ticketDetailPage.ClickAddNote();
+
+        string content = "Novi note iz testa";
+        
+        ticketDetailPage.EnterContent(content);
+        ticketDetailPage.ClickSubmit();
+        
+        Assert.That(ticketDetailPage.GetNoteItemsCount(), Is.EqualTo(countBefore + 1), "There should be exactly 1 note after creating one");
+        Assert.That(ticketDetailPage.IsModalOpened(), Is.Not.True);
+    }
+}
