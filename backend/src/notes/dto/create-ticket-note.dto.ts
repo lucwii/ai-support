1005626ff568c7cbb0cf9d09dcc26b3f330cd41b
lucwii <milanoviclukaa23@gmail.com>
2026-04-01 14:29:37 +0200

    feat: create notes dto file added

---
+++ backend/src/notes/dto/create-ticket-note.dto.ts
@@ -0,0 +1,9 @@
+import { IsString, MaxLength, MinLength } from "class-validator";
+
+export class CreateTicketNoteDto {
+
+    @IsString()
+    @MinLength(1)
+    @MaxLength(5000)
+    content: string;
+}
