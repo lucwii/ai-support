65b6a216ee9e97e25f811fc87ea483fa898289c5
lucwii <milanoviclukaa23@gmail.com>
2026-04-16 15:30:40 +0200

    feat: dashboard tests finished

--- automated-tests/automated-tests/Tests/DashboardTests.cs
+++ automated-tests/automated-tests/Tests/DashboardTests.cs
@@ -26,7 +26,7 @@ public class DashboardTests : TestBase
     [Test]
     public void StatsRow_IsVisible()
     {
-        Assert.That(dashboardPage.StatRows_IsVisible);
+        Assert.That(dashboardPage.StatRows_IsVisible());
     }
 
     [Test]
@@ -38,6 +38,84 @@ public class DashboardTests : TestBase
     [Test]
     public void RecentTickets_MaxTen()
     {
-        Assert.That(dashboardPage.RecentTickets_Count(), Is.LessThan(10));
+        Assert.That(dashboardPage.RecentTickets_Count(), Is.LessThanOrEqualTo(10));
+    }
+
+    // Stat cards
+    [Test]
+    public void TicketsTodayCard_IsVisible()
+    {
+        Assert.That(dashboardPage.TicketsTodayCard_IsVisible());
+    }
+
+    [Test]
+    public void AutoResolvedCard_IsVisible()
+    {
+        Assert.That(dashboardPage.AutoResolvedCard_IsVisible());
+    }
+
+    [Test]
+    public void PendingReviewCard_IsVisible()
+    {
+        Assert.That(dashboardPage.PendingReviewCard_IsVisible());
+    }
+
+    [Test]
+    public void AvgResponseCard_IsVisible()
+    {
+        Assert.That(dashboardPage.AvgResponseCard_IsVisible());
+    }
+
+    [Test]
+    public void AvgResponse_ValueIsAlways2s()
+    {
+        Assert.That(dashboardPage.GetAvgResponseValue(), Is.EqualTo("< 2s"));
+    }
+
+    // Chart
+    [Test]
+    public void TicketsChart_IsVisible()
+    {
+        Assert.That(dashboardPage.TicketsChart_IsVisible());
+    }
+
+    [Test]
+    public void ChartPeriod_SwitchTo7d()
+    {
+        dashboardPage.ClickChartPeriod7d();
+        Assert.That(dashboardPage.IsChartPeriodActive("7d"));
+    }
+
+    [Test]
+    public void ChartPeriod_SwitchTo14d()
+    {
+        dashboardPage.ClickChartPeriod14d();
+        Assert.That(dashboardPage.IsChartPeriodActive("14d"));
+    }
+
+    [Test]
+    public void ChartPeriod_DefaultIs30d()
+    {
+        Assert.That(dashboardPage.IsChartPeriodActive("30d"));
+    }
+
+    // Recent tickets
+    [Test]
+    public void RecentTicketsTable_IsVisible()
+    {
+        Assert.That(dashboardPage.RecentTicketsTable_IsVisible());
+    }
+
+    [Test]
+    public void ViewAllTicketsLink_IsVisible()
+    {
+        Assert.That(dashboardPage.ViewAllTicketsLink_IsVisible());
+    }
+
+    [Test]
+    public void TicketRow_Click_NavigatesToDetail()
+    {
+        dashboardPage.ClickFirstTicketRow();
+        Assert.That(driver.Url, Does.Contain("/dashboard/tickets/"));
     }
 }
